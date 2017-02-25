4b6b383dc90977f6002b970dae116249a5c6efe7
Chabane <chabane.refes@gmail.com>
2017-02-26 00:46:14 +0100

    enable kafka

---
+++ src/main/java/com/mitosis/spring5app/api/SpringBootKafkaController.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mitosis.spring5app.api;
+
+import com.mitosis.spring5app.bean.Listener;
+import java.util.concurrent.ExecutionException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
+import org.springframework.util.concurrent.ListenableFuture;
+import org.springframework.util.concurrent.ListenableFutureCallback;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ * @author root
+ */
+@RestController
+public class SpringBootKafkaController {
+    
+    @Value("${topic}")
+    private String topic;
+    
+    @Autowired
+    private KafkaTemplate<String, String> kafkaTemplate;
+    
+    @Autowired
+    private Listener listener;
+
+    @RequestMapping(method = RequestMethod.GET, path = "/hello")
+    public void hello() throws ExecutionException, InterruptedException {
+        ListenableFuture<SendResult<String, String>> future = kafkaTemplate.send(topic, "Hello world");
+        future.addCallback(new ListenableFutureCallback<SendResult<String, String>>() {
+            @Override
+            public void onSuccess(SendResult<String, String> result) {
+                System.out.println("success");
+            }
+
+            @Override
+            public void onFailure(Throwable ex) {
+                System.out.println("failed");
+            }
+        });
+    }
+}
---
+++ src/main/java/com/mitosis/spring5app/bean/Listener.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mitosis.spring5app.bean;
+
+import java.util.concurrent.CountDownLatch;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.springframework.kafka.annotation.KafkaListener;
+
+/**
+ *
+ * @author root
+ */
+public class Listener {
+
+    public final CountDownLatch countDownLatch1 = new CountDownLatch(1);
+
+    @KafkaListener(id = "foo", topics = "topic-mitosis", group="mitosis")
+    public void listen(ConsumerRecord<?, ?> record) {
+        System.out.println(record);
+        countDownLatch1.countDown();
+    }
+
+}
